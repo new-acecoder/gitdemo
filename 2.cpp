49c91cae1897501128508f89d6f876111eaecad9
Ace <3506647717@qq.com>
2025-01-25 18:27:32 +0800

    implement max value finder in array

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,23 @@
-#include
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+    int max = a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max = a[i];
+        }
+    }
+    cout << max;
+    return 0;
+}
